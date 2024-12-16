e39b7d59fec9853ec5eddcf9263a2e67bae5ddf0
KK-KLA <kk2711111@gmail.com>
2024-12-16 09:58:15 +0700

    ca

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 console.log("Kaokla Suviliai")
 console.log("KLA")
+function appendToDisplay(value) {
+  document.getElementById('display').value += value;
+}
+
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+
+function calculateResult() {
+  const display = document.getElementById('display');
+  try {
+    display.value = eval(display.value); 
+  } catch (e) {
+    display.value = 'Error'; 
+  }
+}
